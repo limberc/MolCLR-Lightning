55b5ab5084be9b4baed682c929921d4a2cd9b5d7
limberc <zehua.cheng@cs.ox.ac.uk>
2024-01-02 07:02:26 +0000

    Update.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pytorch_lightning.cli import LightningCLI
 
-from dataset import MolDataModule
+from mol_data import MolDataModule
 from .molclr import MolCLRModule
 
 if __name__ == '__main__':
-    cli = LightningCLI(MolCLRModule, MolDataModule, save_config_overwrite=True, seed_everything_default=42,
+    cli = LightningCLI(MolCLRModule, MolDataModule, seed_everything_default=42,
                        trainer_defaults={
                            'devices': 2,
                            'accelerator': 'auto',
